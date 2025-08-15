5487469be92b13962fa2cdf7fc5d4d5cdddee181
erineia.silva <erineia.silva@cnj.jus.br>
2025-08-15 16:34:19 -0300

    Aula 3 - modulo 7-Utilizando Mocks

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -3,6 +3,7 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const { from } = require('form-data');
 const app = require('../../app');
+const transferService = require('../../services/transferService')
 
 describe('Transfer Controller', () =>{
   describe('POST /transfer', () =>{
@@ -15,9 +16,27 @@ describe('Transfer Controller', () =>{
           amount: 50
        });
       expect(resposta.status).to.equal(400);
-      expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
+      expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
     });
-  });
+
+    it('Usando Mocks: Quando informo remetente e destinatario inexistentes recebo 400', async () =>{
+      const transferServiceMock = sinon.stub(transferService, 'transfer');
+      transferServiceMock.throws(new Error('Usuário remetente ou destinatário não encontrado'));
+
+      const resposta = await request(app)
+           .post('/transfer')
+           .send({
+               from: "Maria",
+               to: "Neia",
+               amount: 50
+           });
+
+        expect(resposta.status).to.equal(400);
+        expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+
+        sinon.restore();
+    });
+});
   describe('GET /transfers', ()=>{
 
   });
